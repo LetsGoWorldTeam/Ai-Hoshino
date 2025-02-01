8c1dcb910075ccfc64e0eaf777a132fc7b08b09b
LetsGoWorld <iambearbg@gmail.com>
2025-02-01 09:48:39 -0500

    Add files via upload

---
+++ plugins/downloader-apkmod.js
@@ -0,0 +1,25 @@
+let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
+if (!args[0]) throw '[ ✰ ] Ingresa el nombre de la aplicación que deseas descargar junto al comando.\n\n`» Ejemplo :`\n' + `> *${usedPrefix + command}* WhatsApp`, m, rcanal)
+let res = await fetch(`https://api.dorratz.com/v2/apk-dl?text=${args[0]}`);
+let result = await res.json();
+let { name, size, lastUpdate, icon } = result;
+let URL = result.dllink
+let packe = result.package
+let texto = `  ❯───「 𝗔𝗣𝗞 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 」───❮
+    🇷🇺 𝐍𝐨𝐦𝐛𝐫𝐞 : ⇢ ${name} 📩
+    🇷🇺 𝐓𝐚𝐦𝐚𝐧̃𝐨 : ⇢ ${size} ⚖️
+    🇷🇺 𝐏𝐚𝐜𝐤𝐚𝐠𝐞 : ⇢ ${packe} 📦
+    🇷🇺 𝐀𝐜𝐭𝐮𝐚𝐥𝐢𝐳𝐚𝐝𝐨 : ⇢ ${lastUpdate} 🗓️
+    
+## Su aplicación se enviará en un momento POR FAVOR ESPERE . . .`
+await conn.sendFile(m.chat, icon, name + '.jpg', texto, m)
+
+await conn.sendMessage(m.chat, { document: { url: URL }, mimetype: 'application/vnd.android.package-archive', fileName: name + '.apk', caption: ''}, { quoted: m });
+}
+handler.tags = ['downloader']
+handler.help = ['apkmod']
+handler.command = /^(apkmod|apk|dapk2|aptoide|aptoidedl)$/i
+handler.register = true
+handler.zenis = 10
+
+export default handler
