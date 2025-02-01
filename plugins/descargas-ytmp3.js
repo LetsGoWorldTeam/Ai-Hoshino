f45e4c28d01c12ba150ccd33bfac0bfd71f37591
LetsGoWorld <iambearbg@gmail.com>
2025-02-01 11:38:34 -0500

    Add files via upload

---
+++ plugins/descargas-ytmp3.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw '[ ✰ ] Ingresa el enlace del vídeo de *YouTube* junto al comando.\n\n`» Ejemplo :`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`, m, rcanal)}
+
+  await m.react('🕓');
+
+  try {
+    const apiKey = 'xenzpedo';
+    const response = await fetch(`https://api.botcahx.eu.org/api/dowloader/yt?url=${encodeURIComponent(text)}&apikey=${apiKey}`);
+    const result = await response.json();
+
+    if (result.status && result.result && result.result.mp3) {
+      await conn.sendMessage(
+        m.chat,
+        { 
+          audio: { url: result.result.mp3 }, 
+          mimetype: 'audio/mpeg' 
+        },
+        { quoted: m }
+      );
+
+      await m.react('✅');
+    } else {
+      throw new Error('Error: Unable to fetch audio');
+    }
+  } catch (error) {
+    await m.react('❌');
+    m.reply(`❌ *Error:* ${error.message || 'An unknown error occurred'}`);
+  }
+};
+
+handler.help = ['ytmp3 *<url>*']; 
+handler.command = ['ytmp3'];
+handler.tags = ['dl'];
+
+export default handler;
