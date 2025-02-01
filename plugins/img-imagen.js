7eefe991998a81d3aeb311b28af843fe065b7222
LetsGoWorld <iambearbg@gmail.com>
2025-02-01 11:16:56 -0500

    Update img-imagen.js

--- plugins/img-imagen.js
+++ plugins/img-imagen.js
@@ -1,35 +1,22 @@
-import { googleImage } from '@bochilteam/scraper'
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    let user = global.db.data.users[m.sender]
-
-    if (!text) throw `𝗤𝘂𝗲 𝗯𝘂𝘀𝗰𝗮𝗿? 🤔️\n𝗨𝘀𝗲𝗹𝗼 𝗱𝗲 𝗹𝗮 𝘀𝗶𝗴𝘂𝗶𝗲𝗻𝘁𝗲 𝗺𝗮𝗻𝗲𝗿𝗮\n𝗘𝗷𝗲𝗺𝗽𝗹𝗼:\n*${usedPrefix + command} Loli*`
-
-    const res = await googleImage(text)
-    let image = res.getRandom()
-    let link = image
-
-    await delay(1000)
-
-    await conn.sendMessage(m.chat, { 
-        image: { url: link }, 
-        caption: `*🔎 Resultado De: ${text}*`, 
-        footer: dev, 
-        buttons: [
-            {
-                buttonId: `${usedPrefix + command} ${text}`,
-                buttonText: { displayText: 'Siguiente' }
-            }
-        ],
-        viewOnce: true,
-        headerType: 4
-    }, { quoted: m })
-}
-
-handler.help = ['imagen *<búsqueda>*']
-handler.tags = ['img']
-handler.command = ['image', 'gimage', 'imagen']
-
-export default handler
-
-const delay = time => new Promise(res => setTimeout(res, time))
+import {googleImage} from '@bochilteam/scraper';
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+if (!text) return conn.reply(m.chat, '🍬 Por favor, ingresa un término de búsqueda.', m, rcanal);
+await m.react(rwait)
+conn.reply(m.chat, '🍭 Descargando su imagen, espere un momento...', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: dev,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+const res = await googleImage(text);
+const image = await res.getRandom();
+const link = image;
+const messages = [['Imagen 1', dev, await res.getRandom(),
+[[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 3', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
+await conn.sendCarousel(m.chat, `🍬 Resultado de ${text}`, '⪛✰ Imagen - Búsqueda ✰⪜', null, messages, m);
+};
+handler.help = ['imagen'];
+handler.tags = ['buscador', 'tools', 'descargas'];
+handler.command = ['image', 'imagen'];
+handler.register = true
+export default handler;
